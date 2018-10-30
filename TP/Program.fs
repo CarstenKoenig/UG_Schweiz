2633dafe5a66089a49de116a9ad2def6972433c3
Carsten König <carsten@gettingsharper.de>
2018-10-30 10:51:41 +0100

    TP example

---
+++ TP/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+open FSharp.Data
+
+type Stocks = CsvProvider<"./MSFT.csv">
+
+
+[<EntryPoint>]
+let main argv =
+
+  let file = argv.[0]
+  let rows = Stocks.Load(uri=file).Rows
+
+  printfn "Some stock values"
+  for row in Seq.take 10 rows do
+    printfn "%s %.2f" (row.Date.ToString ("dd.MM.yyyy")) row.Open
+
+  0
